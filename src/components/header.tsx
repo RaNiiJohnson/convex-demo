a41816ab5576fb81fada3cdc4e059270b55a966d
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2026-01-05 05:55:46 +0300

    feat: Initialize project with core UI components, theme management, and a refactored `src` directory structure.

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { ThemeToggle } from "./theme-toggle";
+import { buttonVariants } from "./ui/button";
+
+export const Header = () => {
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+      <div className="flex items-center gap-8">
+        <Link href="/">
+          <h1 className="text-xl font-bold">Demo</h1>
+        </Link>
+        <div className="flex items-center gap-2">
+          <Link href="/">Home</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/create">Create</Link>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Link
+          className={buttonVariants({ variant: "default" })}
+          href="/auth/login"
+        >
+          Login
+        </Link>
+        <Link
+          className={buttonVariants({ variant: "outline" })}
+          href="/auth/register"
+        >
+          Register
+        </Link>
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
+};
